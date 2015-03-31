365a56c535163660fa06d0488c9953bc74f0e5b7
Anton Priadko <antonpriadko@gmail.com>
2015-04-01 00:38:37 +0300

    Added create action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :new]
+  before_action :authenticate_user!, only: [:edit, :new, :create, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :check_view_access, only: [:show]
   before_action :check_edit_access, only: [:edit]
@@ -19,6 +19,21 @@ class ItemsController < ApplicationController
   def edit
   end
 
+  def create
+    @item = Item.new item_params
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
 
     def set_item
@@ -33,5 +48,10 @@ class ItemsController < ApplicationController
 
     def check_edit_access
       forbidden unless @item.can_edit? current_user
+    end
+
+    def item_params
+      params.require(:item).permit(:title, :description, :kind, :link, :public)
+            .merge(user_id: current_user.id)
     end
 end
