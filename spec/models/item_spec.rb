b31d2d30c3b3cd3273c459c4e02999b7c7c56d19
Anton Priadko <antonpriadko@gmail.com>
2015-03-31 22:05:58 +0300

    Added test to avoid saving anonymous items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe Item, type: :model do
 
   it('is invalid without link')      { expect(build :image, link: '').not_to be_valid }
 
+  it('is invalid without user')      { expect(build :image, user: nil).not_to be_valid }
+
   it('is invalid without kind')      { expect(build :image, kind: '').not_to be_valid }
   it('is invalid with invalid type') { expect(build :image, kind: 'application').not_to be_valid }
 end
