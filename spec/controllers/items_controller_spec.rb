f0a480d91d3a8e4bc9f0e852276d69c70f23eb14
Anton Priadko <antonpriadko@gmail.com>
2015-03-31 23:34:38 +0300

    Updated tests for index and show actions

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,60 +1,118 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
+  context 'user not signed in' do
+    describe "GET #index" do
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+      it 'populates an array of items' do
+        public_items = [
+          create(:image, public: true, user: find_or_create(:bob)),
+          create(:image, public: true, user: find_or_create(:alice))
+        ]
+
+        get :index
+        expect(assigns :my_items).to eq([])
+        expect(assigns :public_items).to eq(public_items)
+      end
+
+      it 'renders index template' do
+        get :index
+        expect(response).to render_template :index
+      end
+
     end
 
-    it 'populates an array of items' do
-      items = [create(:image), create(:video)]
-      get :index
-      expect(assigns :items).to eq(items)
-    end
+    describe "GET #show" do
+      let(:item) {create(:image, public: true)}
 
-    it 'renders index template' do
-      get :index
-      expect(response).to render_template :index
+      it "returns http success" do
+        get :show, id: item.id
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns 404 page if not exist" do
+        get :show, id: "la_la_la"
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "redirects to sign in page if not public" do
+        item.update public: false
+        get :show, id: item.id
+        expect(response).to redirect_to '/users/sign_in'
+      end
+
+      it 'populates an item' do
+        get :show, id: item.id
+        expect(assigns :item).to eq(item)
+      end
+
+      it 'renders show template' do
+        get :show, id: item.id
+        expect(response).to render_template :show
+      end
     end
   end
 
-  describe "GET #show" do
-    let(:item) {create(:image, public: true)}
+  context 'user signed in' do
+    before { sign_in find_or_create :alice }
 
-    it "returns http success" do
-      get :show, id: item.id
-      expect(response).to have_http_status(:success)
+    describe "GET #index" do
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'populates an array of items' do
+        public_items = [create(:image, public: true, user: find_or_create(:bob))]
+        my_items = [
+          create(:image, public: true, user: find_or_create(:alice)),
+          create(:image, public: false, user: find_or_create(:alice))
+        ]
+        create(:image, public: false, user: find_or_create(:bob))
+
+        get :index
+        expect(assigns :my_items).to eq(my_items)
+        expect(assigns :public_items).to eq(public_items)
+      end
+
+      it 'renders index template' do
+        get :index
+        expect(response).to render_template :index
+      end
     end
 
-    it "returns 404 page if not exist" do
-      get :show, id: "la_la_la"
-      expect(response).to have_http_status(:not_found)
-    end
+    describe "GET #show" do
+      let(:item) {create(:image, public: false, user: find_or_create(:alice))}
 
-    it "returns 403 page if not public" do
-      item.update public: false
-      get :show, id: item.id
-      expect(response).to have_http_status(:forbidden)
-    end
+      it "returns http success" do
+        get :show, id: item.id
+        expect(response).to have_http_status(:success)
+      end
 
-    it 'populates an array of items' do
-      get :show, id: item.id
-      expect(assigns :item).to eq(item)
-    end
+      it "returns 404 page if not exist" do
+        get :show, id: "la_la_la"
+        expect(response).to have_http_status(:not_found)
+      end
 
-    it 'renders index template' do
-      get :show, id: item.id
-      expect(response).to render_template :show
-    end
-  end
+      it "returns 403 page if not public for signed in users" do
+        item.update public: false, user: find_or_create(:bob)
+        get :show, id: item.id
+        expect(response).to have_http_status(:forbidden)
+      end
 
+      it 'populates an item' do
+        get :show, id: item.id
+        expect(assigns :item).to eq(item)
+      end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+      it 'renders show template' do
+        get :show, id: item.id
+        expect(response).to render_template :show
+      end
     end
   end
 end
