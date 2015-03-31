668ae6999112b0ac7433696826d310b81152caab
Anton Priadko <antonpriadko@gmail.com>
2015-03-31 19:45:53 +0300

    Added validations for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ActiveRecord::Base
+  ALLOWED_MEDIA_TYPES = %w(video image)
+
+  validates :title, :link, :kind, presence: true
+  validates :kind, inclusion: {in: ALLOWED_MEDIA_TYPES}
 end
