a5b979ac10f121815a5881c06e3cc9d2f65163da
Anton Priadko <antonpriadko@gmail.com>
2015-03-31 21:39:01 +0300

    Added devise to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'devise'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -51,6 +54,7 @@ RSpec.configure do |config|
     end
   end
 
+  config.include Devise::TestHelpers, :type => :controller
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
