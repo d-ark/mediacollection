fe21c3b0245abbe8ee285bdfb01cb0ab96eaf09f
Anton Priadko <antonpriadko@gmail.com>
2015-03-31 20:15:50 +0300

    Added basic tests for items#index

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe ItemsController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it 'populates an array of items' do
+      items = [create(:image), create(:video)]
+      get :index
+      expect(assigns :items).to eq(items)
+    end
+
+    it 'renders index template' do
+      get :index
+      expect(response).to render_template :index
+    end
   end
 
   describe "GET #new" do
