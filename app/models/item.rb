012f002b50d0ab795b2d066cafedde5e09ea7e12
Anton Priadko <antonpriadko@gmail.com>
2015-03-31 22:11:13 +0300

    Added user validations for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ActiveRecord::Base
   ALLOWED_MEDIA_TYPES = %w(video image)
+  belongs_to :user
 
-  validates :title, :link, :kind, presence: true
+  validates :title, :link, :kind, :user, presence: true
   validates :kind, inclusion: {in: ALLOWED_MEDIA_TYPES}
 end
