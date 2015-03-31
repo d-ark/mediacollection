e8f15390d65b818d10147f5c1fbe1e5d2bf8f8a2
Anton Priadko <antonpriadko@gmail.com>
2015-03-31 20:45:00 +0300

    Added test for show item

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -20,6 +20,26 @@ RSpec.describe ItemsController, type: :controller do
     end
   end
 
+  describe "GET #show" do
+    let(:item) {create(:image)}
+
+    it "returns http success" do
+      get :show, id: item.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'populates an array of items' do
+      get :show, id: item.id
+      expect(assigns :item).to eq(item)
+    end
+
+    it 'renders index template' do
+      get :show, id: item.id
+      expect(response).to render_template :show
+    end
+  end
+
+
   describe "GET #new" do
     it "returns http success" do
       get :new
