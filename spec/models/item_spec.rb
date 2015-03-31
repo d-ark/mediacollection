efdc3ce1098e87e07abf0bc1f9ecfc65791a01a7
Anton Priadko <antonpriadko@gmail.com>
2015-03-31 19:43:22 +0300

    Added tests for model validations

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Item, type: :model do
   it('has valid factories') { expect(build :image).to be_valid }
   it('has valid factories') { expect(build :video).to be_valid }
 
+  it('is invalid without title')     { expect(build :image, title: '').not_to be_valid }
+
+  it('is invalid without link')      { expect(build :image, link: '').not_to be_valid }
+
+  it('is invalid without kind')      { expect(build :image, kind: '').not_to be_valid }
+  it('is invalid with invalid type') { expect(build :image, kind: 'application').not_to be_valid }
 end
